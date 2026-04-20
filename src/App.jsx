df87bdefdf73c22f4f2888a68d3d2395bf857b49
kiendt298 <kiendt298@gmail.com>
2026-04-21 01:01:35 +0700

    -2,45

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const ENV_CONFIG = {
  room1: {
     envScale:     35,
     centerOffset: { x: -15, z: 0 }, // khoảng cách camera với model
-    cameraPos:    { x: -13, y: -2.3, z:  0 },  // vị trí của camrera
+    cameraPos:    { x: -13, y: -2.45, z:  0 },  // vị trí của camrera
     cameraTarget: { x: -25, y: -5, z: 0 }, // hướng của camrera
     modelPos:     { x: -35, y: -4.25, z: 0 }, // vị trí của model
     modelRotY:    14.2                         // model quay mặt về phía camera       
